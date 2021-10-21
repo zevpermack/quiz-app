1b87619a1c9ba8b22ee62661390158d054bb2d1e
rewpt <zpermack@gmail.com>
2021-10-21 18:51:09 +0000

    quizzes route made

--- server.js
+++ server.js
@@ -43,13 +43,14 @@ app.use(express.static("public"));
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
 const usersRoutes = require("./routes/users");
-const widgetsRoutes = require("./routes/widgets");
 const loginRoutes = require("./routes/login");
+const quizRoutes = require("./routes/quizzes");
 const { DataRowMessage } = require("pg-protocol/dist/messages");
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 app.use("/api/users", usersRoutes(db));
 app.use("/login", loginRoutes(db));
+app.use("/quizzes", quizRoutes(db));
 // Note: mount other resources here, using the same pattern above
 
 // Home page
