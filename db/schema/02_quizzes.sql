3f05b2ad07806bf20ec3bfc424e936d749e79806
rewpt <zpermack@gmail.com>
2021-10-21 18:35:58 +0000

    added date_created to quiz schema

--- db/schema/02_quizzes.sql
+++ db/schema/02_quizzes.sql
@@ -6,6 +6,6 @@ CREATE TABLE quizzes (
   id SERIAL PRIMARY KEY NOT NULL,
   user_id INTEGER REFERENCES users(id),
   title VARCHAR(255) NOT NULL,
-  visibility VARCHAR(255) DEFAULT 'public'
-
+  visibility VARCHAR(255) DEFAULT 'public',
+  date_created DATE DEFAULT CURRENT_DATE
 );
