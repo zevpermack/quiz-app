1c80d35aaaefdc5ee0b1505ba94f56d7f9fc9ae4
Fatuma <sfatex@gmail.com>
2021-10-21 21:57:09 -0400

    :wqMake query and route for handling all quizzes and quiz by id

--- server.js
+++ server.js
@@ -14,7 +14,9 @@ const cookieSession = require('cookie-session');
 const { Pool } = require("pg");
 const dbParams = require("./lib/db.js");
 const db = new Pool(dbParams);
+
 db.connect();
+
 
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
@@ -41,17 +43,17 @@ app.use(
 app.use(express.static("public"));
 
 // Separated Routes for each Resource
-// Note: Feel free to replace the example routes below with your own
+
 const usersRoutes = require("./routes/users");
 const loginRoutes = require("./routes/login");
-const quizRoutes = require("./routes/quizzes");
+const quizRoutes = require("./routes/quizRouters");
 const { DataRowMessage } = require("pg-protocol/dist/messages");
 // Mount all resource routes
-// Note: Feel free to replace the example routes below with your own
+
 app.use("/api/users", usersRoutes(db));
 app.use("/login", loginRoutes(db));
 app.use("/quizzes", quizRoutes(db));
-// Note: mount other resources here, using the same pattern above
+
 
 // Home page
 // Warning: avoid creating more routes in this file!
@@ -78,3 +80,5 @@ app.get("/", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
+
+// module.exports = db
