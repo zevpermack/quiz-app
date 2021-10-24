c0d03c649ccff5a3d92d7d368aa02590d5e03d87
rewpt <zpermack@gmail.com>
2021-10-24 05:11:51 +0000

    add name differentiation for radio buttons grouped by quesiton

--- public/scripts/create.js
+++ public/scripts/create.js
@@ -17,20 +17,20 @@ $(() => {
         <label for="question${questionNumber}">Question ${questionNumber}:</label>
         <input class="form-control mb-2" type="text" name="question${questionNumber}" placeholder="eg. What is the capital of?" style= "width: 1000px;">
         <div class="form-check">
-          <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadio${questionNumber}1">
-          <input class="form-control" for="flexRadio${questionNumber}1" type="text" name="question1" placeholder="Choice 1" style= "width: 500px;">
+          <input class="form-check-input" type="radio" name="radio${questionNumber}" id="flexRadio${questionNumber}1">
+          <input class="form-control" for="flexRadio${questionNumber}1" type="text" name="question${questionNumber}" placeholder="Choice 1" style= "width: 500px;">
         </div>
         <div class="form-check">
-          <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadio${questionNumber}2">
-          <input class="form-control" for="flexRadio${questionNumber}2" type="text" name="question1" placeholder="Choice 2" style= "width: 500px;">
+          <input class="form-check-input" type="radio" name="radio${questionNumber}" id="flexRadio${questionNumber}2">
+          <input class="form-control" for="flexRadio${questionNumber}2" type="text" name="question${questionNumber}" placeholder="Choice 2" style= "width: 500px;">
         </div>
         <div class="form-check">
-          <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadio${questionNumber}3">
-          <input class="form-control" for="flexRadio${questionNumber}3" type="text" name="question1" placeholder="Choice 3" style= "width: 500px;">
+          <input class="form-check-input" type="radio" name="radio${questionNumber}" id="flexRadio${questionNumber}3">
+          <input class="form-control" for="flexRadio${questionNumber}3" type="text" name="question${questionNumber}" placeholder="Choice 3" style= "width: 500px;">
         </div>
         <div class="form-check">
-          <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadio${questionNumber}4">
-          <input class="form-control" for="flexRadio${questionNumber}4" type="text" name="question1" placeholder="Choice 4" style= "width: 500px;">
+          <input class="form-check-input" type="radio" name="radio${questionNumber}" id="flexRadio${questionNumber}4">
+          <input class="form-control" for="flexRadio${questionNumber}4" type="text" name="question${questionNumber}" placeholder="Choice 4" style= "width: 500px;">
         </div>
       </div>
       `
