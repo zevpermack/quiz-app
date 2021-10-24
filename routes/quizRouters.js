8c0215cf38908cb9228f18180429cec5edc33981
Fatuma <sfatex@gmail.com>
2021-10-23 21:36:03 -0400

    update the /quizzes route to render the quizzes.ejs with the username and all public quizzes

--- routes/quizRouters.js
+++ routes/quizRouters.js
@@ -1,49 +1,58 @@
 const express = require('express');
 const router = express.Router();
 
-const { getAllQuizzes,getQuizById,createNewQuestions,createNewQuiz } = require('./../db/quizQueries.js');
+const { getAllQuizzes, getQuizById, createNewQuestions, createNewQuiz } = require('./../db/quizQueries.js');
 
 const quizRouters = (db) => {
 
 
-  router.get('/',(req,res) =>{
-    getAllQuizzes(db)
-    .then((quizzes) =>{
-      console.log(quizzes)
-      return res.json(quizzes)
-    })
+  router.get('/', (req, res) => {
+    const userId = req.session.user_id;
+
+    if (userId) {
+      return db.query("SELECT * FROM users  WHERE users.id = $1 ", [userId])
+        .then((loginData) => {
+          getAllQuizzes(db)
+            .then((quizzes) => {
+              console.log(quizzes)
+              res.render("quizzes", { data: quizzes, name: loginData.rows[0].name })
+            })
+        }).catch((err) => {
+          console.log(err.message);
+        })
+    }
   })
 
 
-  router.get('/new',(req,res) =>{
-   res.render("test_new_quiz")
+  router.get('/new', (req, res) => {
+    res.render("test_new_quiz")
 
 
   })
 
-  router.get('/:id',(req,res) =>{
-    getQuizById(db,req.params.id)
-    .then((quiz) =>{
-      return res.json(quiz)
-    })
+  router.get('/:id', (req, res) => {
+    getQuizById(db, req.params.id)
+      .then((quiz) => {
+        return res.json(quiz)
+      })
   })
 
-  router.post('/',(req,res) =>{
+  router.post('/', (req, res) => {
     // console.log(req.body)
     // res.send("ok");
-    const obj={};
-   createNewQuiz(db,obj)
-   .then((data) =>{
-    //loop through user questions and run createnewquestion for every question
+    const obj = {};
+    createNewQuiz(db, obj)
+      .then((data) => {
+        //loop through user questions and run createnewquestion for every question
 
-   })
-   })
+      })
+  })
 
-router.post('/:id',(req,res) => {
+  router.post('/:id', (req, res) => {
 
-})
+  })
 
-return router;
+  return router;
 
 }
 module.exports = quizRouters;
