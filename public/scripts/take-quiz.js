d8da87744081d822c734fe4e18a79fbba0795df9
rewpt <zpermack@gmail.com>
2021-10-25 18:41:47 +0000

    add taking-quiz view dynamically loaded in js with stylings. Still needs backend hookup

---
+++ public/scripts/take-quiz.js
@@ -0,0 +1,54 @@
+$(() => {
+  const title = 'Quiz Title';
+  const questions = {
+    'What is 1+1' : ['0', '1', '2', '3',],
+    'What is 2+2' : ['0', '2', '5', '4',],
+    'What is 3+3' : ['5', '6', '7', '8',],
+    'What is 4+4' : ['8', '9', '10', '11',],
+  }
+  console.log('this is take-quiz.js');
+  $('.main-grid').prepend(`
+  <h1 id = "page-header">${title}</h1>
+  `
+  );
+
+  let qCount = 1;
+  for(question in questions) {
+    console.log(`run ${qCount}`);
+    $('#question-form').append(
+      `
+      <div id = q${qCount}>
+          <h2 class = "question-title" id = "q-title-${qCount}">${qCount}. ${question}</h2>
+          <div class="form-check">
+            <input class="form-check-input" type="radio" name="Radio${qCount}" id="answer${qCount}1" value="option1" checked>
+            <label class="form-check-label" for="Radio${qCount}">
+              ${questions[question][0]}
+            </label>
+          </div>
+          <div class="form-check">
+            <input class="form-check-input" type="radio" name="Radio${qCount}" id="exampleRadios${qCount}2" value="option2">
+            <label class="form-check-label" for="Radio${qCount}">
+            ${questions[question][1]}
+            </label>
+          </div>
+          <div class="form-check">
+            <input class="form-check-input" type="radio" name="Radio${qCount}" id="exampleRadios${qCount}3" value="option3">
+            <label class="form-check-label" for="Radio${qCount}">
+            ${questions[question][2]}
+            </label>
+          </div>
+          <div class="form-check">
+            <input class="form-check-input" type="radio" name="Radio${qCount}" id="exampleRadios${qCount}4" value="option4">
+            <label class="form-check-label" for="Radio${qCount}">
+            ${questions[question][3]}
+            </label>
+          </div>
+        </div>
+      `
+    );
+    qCount++;
+  }
+  $('#question-form').append(
+  `<div id = "btn-container"><input class="btn mt-2" id = "submit-btn" type="submit" value="Submit"></input></div>`
+  )
+})
