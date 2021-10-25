b2e1940f3b5704d7773c0f4f09a5432b50684697
rewpt <zpermack@gmail.com>
2021-10-25 16:51:51 +0000

    route for quizzes.js

--- routes/quizzes.js
+++ routes/quizzes.js
@@ -39,5 +39,22 @@ module.exports = (db) => {
     }
   });
 
+  router.get('/', (req, res) => {
+    const userId = req.session.user_id
+    if(userId) {
+      return db.query("SELECT * FROM users WHERE id = $1", [userId])
+      .then((data) => {
+        console.log('data rows: ', data.rows[0].name);
+        const templateVars = {name: data.rows[0].name};
+        res.render("quizzes", templateVars);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      })
+    } else {
+      res.send("you must be logged in to create a page");
+    }
+  });
+
   return router;
 };
